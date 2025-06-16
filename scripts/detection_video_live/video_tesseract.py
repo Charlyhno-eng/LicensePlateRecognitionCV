64bc0de352bb43a18f2852d7c2b2b883aacdacc1
Charlyhno-eng <charly7902@gmail.com>
2025-06-16 11:06:41 +0200

    increase the efficiency of the preprocess of the plate

--- scripts/detection_video_live/video_tesseract.py
+++ scripts/detection_video_live/video_tesseract.py
@@ -24,13 +24,13 @@ def preprocess_plate(plate_crop):
     """
     Applies a set of preprocessing steps to enhance plate image for OCR.
     """
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    clahe = cv2.createCLAHE(clipLimit=1.8, tileGridSize=(8,8))
     gray = cv2.cvtColor(plate_crop, cv2.COLOR_BGR2GRAY)
     gray = clahe.apply(gray)
-    gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
-    blur = cv2.bilateralFilter(gray, 11, 17, 17)
-    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    gray = cv2.resize(gray, None, fx=1.4, fy=1.4, interpolation=cv2.INTER_LINEAR)
+    blur = cv2.bilateralFilter(gray, 11, 16, 16)
+    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 13, 2)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
     morph = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
     return morph
 
