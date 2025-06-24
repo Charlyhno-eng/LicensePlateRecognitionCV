a2d66325fd4446f7f035e050da1b6ffc7360f05b
Charlyhno-eng <charly7902@gmail.com>
2025-06-24 15:29:07 +0200

    test onnx - error

---
+++ scripts/detection/test_onnx/video_yolo_test.py
@@ -0,0 +1,30 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO("yolov8mymodel.pt")
+
+def display_camera_with_detection():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            continue
+
+        results = model.predict(source=frame, conf=0.25, imgsz=640, verbose=False)
+        for result in results:
+            for box in result.boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+        cv2.imshow("Camera with Plate Detection", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyWindow("Preprocessed Plate")
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    display_camera_with_detection()
--- scripts/model/pt_to_onnx.py
+++ scripts/model/pt_to_onnx.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8mymodel.pt")
 
-model.export(format="onnx")
+model.export(format="onnx", opset=12, dynamic=False, imgsz=640)
